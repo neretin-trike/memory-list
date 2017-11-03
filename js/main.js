825a36e6e8c4218047d5e631604ce41de048944d
Artem Neretin <hawktrike@gmail.com>
2017-11-03 11:17:18 +0700

    Добавил анимацию раскрывания  списка вместо элемента details

--- js/main.js
+++ js/main.js
@@ -3,3 +3,41 @@ window.SmoothScrollOptions = {
 } 
 SmoothScroll({ stepSize: 75 })
 
+$("p").click(function() {
+
+    var widgetLinks = $(this).parent().children(".widget-links");
+    var beginHeight = widgetLinks.css("height");
+
+    if ($(this).hasClass("opened")){
+        $(this).removeClass("opened");
+
+        widgetLinks.animate({
+                height: 0,
+                opacity: "0" 
+            }, 250, "easeOutCubic",             
+                (function(){
+                    widgetLinks.css( "display", "none");
+                    widgetLinks.css( "height", "auto");
+                })
+        );
+
+    }
+    else{
+        $(this).addClass("opened");
+
+        widgetLinks.css( "height", "0");
+        widgetLinks.css( "display", "block");
+    
+        widgetLinks.animate({
+                height: beginHeight,
+                opacity: "1" 
+            }, 250, "easeOutCubic",             
+                (function(){
+                    widgetLinks.css( "height", "auto");
+                }) 
+        );
+    }
+    
+
+
+  });
