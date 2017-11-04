357773e6ee5a13b662147e68027a5399cc5f6e05
neretin-trike <hawktrike@gmail.com>
2017-11-04 18:36:58 +0700

    Изменил структуру JSON файла добавив второй уровень вложенности

--- js/main.js
+++ js/main.js
@@ -5,30 +5,31 @@ SmoothScroll({ stepSize: 75 })
 
 window.onload = function() {
     $.getJSON("js/list.json", function(json) {
-        var items = [];
-        // console.log(json); // this will show the info it in firebug console
+        var items = [],
+            caption = "";
+   
+        for(var k in json) {
 
-        // for(var k in json) {
-        //     console.log(json[k]);
-        //     console.log(json[k][0]);
-        // }
+            caption = json[k]["caption"];
+            for (var i = 0; i<json[k]["list"].length-1; i+=2){
+                href = json[k]["list"][i];
+                val = json[k]["list"][i+1];
+    
+                items.push('<li><a href="' + href + '">' + val + '</a></li>');
+            }
+    
+            var sec = $("<section class='widget-category'></section>");
+            var ps = $("<p>", {html: caption});
+            var art = $("<article class='widget-links'>");  
+            var ols = $('<ol/>', {html: items.join('')});
+    
+            $(sec).append(ps).append(art);
+            $(art).append(ols);
+    
+            $('.wrapper').append(sec); 
 
-        for (var i = 0; i<json["list"].length-1; i+=2){
-            href = json["list"][i];
-            val = json["list"][i+1];
-
-            items.push('<li><a href="' + href + '">' + val + '</a></li>');
+            items = [];
         }
-
-        var sec = $("<section class='widget-category'></section>");
-        var ps = $("<p>ШПАРГАЛКИ</p>");
-        var art = $("<article class='widget-links'>");  
-        var ols = $('<ol/>', {html: items.join('')});
-
-        $(sec).append(ps).append(art);
-        $(art).append(ols);
-
-        $('.wrapper').append(sec); 
     });
 };
 
@@ -69,3 +70,9 @@ $(".wrapper").on('click','p', function(){
     }
   });
 
+     // console.log(json); // this will show the info it in firebug console
+
+        // for(var k in json) {
+        //     console.log(json[k]);
+        //     console.log(json[k][0]);
+        // }
