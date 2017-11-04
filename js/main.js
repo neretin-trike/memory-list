0a52b67bd0889742f76dc1b3eb6e65853e048144
neretin-trike <hawktrike@gmail.com>
2017-11-04 17:01:28 +0700

    Добавил формирование списка из файла JSON и его чтение с сервера

--- js/main.js
+++ js/main.js
@@ -3,8 +3,46 @@ window.SmoothScrollOptions = {
 } 
 SmoothScroll({ stepSize: 75 })
 
-$("p").click(function() {
+window.onload = function() {
+    $.getJSON("js/list.json", function(json) {
+        var items = [];
+        // console.log(json); // this will show the info it in firebug console
 
+        // for(var k in json) {
+        //     console.log(json[k]);
+        //     console.log(json[k][0]);
+        // }
+
+        for (var i = 0; i<json["list"].length-1; i+=2){
+            href = json["list"][i];
+            val = json["list"][i+1];
+
+            items.push('<li><a href="' + href + '">' + val + '</a></li>');
+        }
+
+        var sec = $("<section class='widget-category'></section>");
+        var ps = $("<p class='opened'>ШПАРГАЛКИ</p>");
+        var art = $("<article class='widget-links'>");  
+        var ols = $('<ol/>', {html: items.join('')});
+
+        $(sec).append(ps).append(art);
+        $(art).append(ols);
+
+        $('.wrapper').append(sec); 
+
+        // json.forEach(function(el) {
+        //     items.push('<li>' + el + '</li>');
+        // }, this);
+
+        // $.each(json, function(key, val) {
+        //   items.push('<li id="' + key + '">' + val + '</li>');
+        // });
+
+    });
+};
+
+$("p").click(function() {
+    
     var widgetLinks = $(this).parent().children(".widget-links");
     var beginHeight = widgetLinks.css("height");
 
@@ -36,8 +74,7 @@ $("p").click(function() {
                     widgetLinks.css( "height", "auto");
                 }) 
         );
+
     }
-    
-
-
   });
+
